118468acfc979ed4cf98dd76f6a63d84b0d837e0
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2018-06-16 00:34:30 +0200

    lat és long konzolban

--- public/javascripts/new.js
+++ public/javascripts/new.js
@@ -3,8 +3,25 @@ $(document).ready(function() {
 	var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: {lat: -34.397, lng: 150.644}
-  });
-  
+	});
+
+	var marker = new google.maps.Marker({
+		position: {lat: -34.397, lng: 150.644}
+	});  
+		marker.setMap(map);
+	
+	google.maps.event.addListener(map, "click", function (e) {
+
+    //lat and lng is available in e object
+		var latLng = e.latLng;
+		
+		console.log(e.latLng.lat())
+		console.log(e.latLng.lng())
+
+		marker.setPosition(latLng);
+
+	});
+	
   var geocoder = new google.maps.Geocoder();
 
   document.getElementById('submit').addEventListener('click', function() {
@@ -21,8 +38,11 @@ $(document).ready(function() {
 	        map: resultsMap,
 	        position: results[0].geometry.location
 	      });
-	      document.getElementById('latitude').value = results[0].geometry.location.lat();
-	      document.getElementById('longitude').value = results[0].geometry.location.lng();
+	      // document.getElementById('latitude').value = results[0].geometry.location.lat();
+				// document.getElementById('longitude').value = results[0].geometry.location.lng();
+				
+				document.getElementById('latitude').value = e.latLng.lat();
+		document.getElementById('longitude').value = e.latLng.lng();
 	    } else {
 	      alert('Geocode was not successful for the following reason: ' + status);
 	    }
